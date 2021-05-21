adbe9b36779602f4b85cd4c781327472b8e131d7
Roger-wsy <rogerwong20011021@gmail.com>
2021-05-21 16:03:30 +0800

    modify event1

--- SocialPath/src/sp/Event1.java
+++ SocialPath/src/sp/Event1.java
@@ -7,19 +7,7 @@ import java.util.Scanner;
 public class Event1 {
     public static void main(String[] args) {
         Relationship r = new Relationship();
-        r.addPerson("v1");
-        r.addPerson("v2");
-        r.addPerson("v3");
-        Person v1 = r.getPerson("v1");
-        Person v2 = r.getPerson("v2");
-        Person v3 = r.getPerson("v3");
-        v1.addF(v2, 1);
-        r.addFriend(v2, v1, 3);
-        r.addFriend(v1, v3, 3);
-        r.addFriend(v2, v3, 3);
-        r.addFriend(v3, v1, 3);
-        r.getPerson("v3").addF(v2, 5);
-        System.out.println(r.toString());
+
 
         labQuestions(r);
         System.out.println(r.toString());
@@ -37,11 +25,20 @@ public class Event1 {
         array1[2] = array[2]; //good/bad
         array1[1] = array[4]; //Person B
         //
+        if(!r.contains(array1[0])){
+            r.addPerson(array1[0]);
+        }
+        if(!r.contains(array1[1])){
+            r.addPerson(array1[1]);
+        }
+        Person personA = r.getPerson(array1[0]);
+        Person personB = r.getPerson(array1[1]);
 
 
+        personA.addF(personB, 0);
+        personB.addF(personA, 0);
         try {
-            Person personA = r.getPerson(array1[0]);
-            Person personB = r.getPerson(array1[1]);
+
             if(array1[2].equals("good")){
                 int AtoB=0;
                 for (int i = 0; i < personA.friend.size(); i++) {
@@ -58,8 +55,7 @@ public class Event1 {
                         AtoB=i;
                     }
                 }
-                newWeight =  personA.friend.get(AtoB).getLove()-2;
-                personA.setF(personB, newWeight);
+                personA.setF(personB, 2);
             }
 
         } catch (NoSuchElementException e) {
