0c3bc73bbc7d8bdf17b5b09d0c08f5624013f904
Nee Kee <76592929+neekee2001@users.noreply.github.com>
2021-06-14 17:42:18 +0800

    Update Event1.java

--- Sociopath/src/sociopath/Event1.java
+++ Sociopath/src/sociopath/Event1.java
@@ -13,21 +13,24 @@ public class Event1 {
     // p2 is teacher, p1 is student, rate is good or not
     public static void askQuestion(Person p1, Person p2, boolean rate){
         System.out.println("Event 1 running:");
-        if (p2.containF(p1)||p1.containF(p2)){
-            System.out.println(p2.getName()+" and "+p1.getName()+" are not strangers"+".");
+        // if they are not strangers, event 1 will not run because the question mentions that the person seeking help is a stranger.
+        if (p2.containF(p1)||p1.containF(p2)){ //check whether they are friends
+            System.out.println(p2.getName()+" and "+p1.getName()+" are not strangers"+"."); //if there are friends, print message
         }
-        else {
+        else { //they are strangers
             System.out.println(p1.getName()+" asks "+p2.getName()+" about lab questions"+".");
+             //print the reputation berfore teaching
             System.out.println("Before teaching, "+p1.getName()+" has "+p1.getRep(p2)+" reputation point towards "+p2.getName()+".");
-            p1.addF(p2, 1);
-            p2.addF(p1, 1);
-            if (rate){
-                p1.setRep(p2, 10);
+            p1.addF(p2, 1); //make them friends
+            p2.addF(p1, 1); //make them friends
+            //true=teaches well, false=teaches badly
+            if (rate){ //teaches well
+                p1.setRep(p2, 10); //reputation point +10
                 System.out.println(p2.getName()+" teaches "+p1.getName()+" very well.");
                 System.out.println("After teaching, "+p1.getName()+" has "+p1.getRep(p2)+" reputation points towards "+p2.getName()+".");
             }
-            else {
-                p1.setRep(p2, 2);
+            else { //teaches badly
+                p1.setRep(p2, 2); //reputation point +2
                 System.out.println(p2.getName()+" teaches "+p1.getName()+" badly.");
                 System.out.println("After teaching, "+p1.getName()+" has "+p1.getRep(p2)+" reputation points towards "+p2.getName()+".");
             }
